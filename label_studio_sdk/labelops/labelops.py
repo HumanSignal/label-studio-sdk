13ea6813192c4f3300346eb25a77fb02ab6bf4c8
nik <nik@heartex.net>
2022-01-19 15:21:04 +0000

    Add example and labelops headers

--- label_studio_sdk/labelops/labelops.py
+++ label_studio_sdk/labelops/labelops.py
@@ -1,25 +1,31 @@
 import requests
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class LabelOps(BaseModel):
     project_id: int
     base_url: str = 'http://stage-21.heartex.ai:9092'
-    api_key: str = None
+    api_key: str = ''
+
+    @validator('api_key')
+    def valid_api_key(cls, v, values, **kwargs):
+        response = requests.get(f'{values["base_url"]}', headers={'API_KEY': v})
+        response.raise_for_status()
+        return v
 
     def initialize_project(self):
-        response = requests.get(f'{self.base_url}/{self.project_id}/process')
+        response = requests.get(f'{self.base_url}/{self.project_id}/process', headers={'API_KEY': self.api_key})
         return response.json()
 
     def check_project_status(self):
-        response = requests.get(f'{self.base_url}/{self.project_id}/status')
+        response = requests.get(f'{self.base_url}/{self.project_id}/status', headers={'API_KEY': self.api_key})
         return response.content
 
     def apply(self, parameters):
-        response = requests.post(f'{self.base_url}/{self.project_id}/labelops', json=parameters)
+        response = requests.post(f'{self.base_url}/{self.project_id}/labelops', json=parameters, headers={'API_KEY': self.api_key})
         return response.json()
 
     def commit(self, result, model_version):
-        response = requests.post(f'{self.base_url}/{self.project_id}/commit/{model_version}', json=result)
+        response = requests.post(f'{self.base_url}/{self.project_id}/commit/{model_version}', json=result, headers={'API_KEY': self.api_key})
         return response.json()
