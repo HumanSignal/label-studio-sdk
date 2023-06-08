84fca61389bfd01375969c49d1a12933edd8f655
makseq-ubnt <makseq@gmail.com>
2023-06-08 02:34:45 +0100

    fix: User creation when username is empty

--- label_studio_sdk/client.py
+++ label_studio_sdk/client.py
@@ -277,7 +277,7 @@ class Client(object):
 
         payload = (
             {
-                'username': user.username,
+                'username': user.username if user.username else user.email,
                 'email': user.email,
                 'first_name': user.first_name,
                 'last_name': user.last_name,
