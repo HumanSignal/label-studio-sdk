bf4fa426c70c60d7bf6869836dddd3f403525e21
makseq-ubnt <makseq@gmail.com>
2024-03-12 02:41:07 +0000

    Add more storage calls

--- label_studio_sdk/__init__.py
+++ label_studio_sdk/__init__.py
@@ -5,9 +5,7 @@ from .client import Client
 from .project import Project
 from .utils import parse_config
 
-__pdoc__ = {
-    "label_interface": False
-}
+__pdoc__ = {"label_interface": False}
 
 
 __version__ = "0.0.34dev0"
