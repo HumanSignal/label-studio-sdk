3337f46e172431f19a7333af100589447eb778fb
nik <nik@heartex.net>
2024-06-04 13:48:29 +0100

    Add converter tests

---
+++ tests/custom/converter_tests/test_export_csv.py
@@ -0,0 +1,51 @@
+import json
+import os
+
+
+from label_studio_sdk.converter import Converter
+from pandas import read_csv
+
+
+def test_simple_csv_export():
+    # Test case 1, simple output, no JSON
+    converter = Converter({}, '/tmp')
+    output_dir = '/tmp/lsc-pytest'
+    result_csv = output_dir + '/result.csv'
+    input_data = os.path.abspath(os.path.dirname(__file__)) + '/data/test_export_csv/csv_test.json'
+    sep = ','
+    converter.convert_to_csv(input_data, output_dir, sep=sep, header=True, is_dir=False)
+
+    df = read_csv(result_csv, sep=sep)
+    nulls = df.isnull().sum()
+    if nulls.any() > 0:
+        assert False, "There should be no empty values in result CSV"
+
+
+def test_csv_export_complex_fields_with_json():
+    converter = Converter({}, '/tmp')
+    output_dir = '/tmp/lsc-pytest'
+    result_csv = output_dir + '/result.csv'
+    input_data = os.path.abspath(os.path.dirname(__file__)) + '/data/test_export_csv/csv_test2.json'
+    assert_csv = os.path.abspath(os.path.dirname(__file__)) + '/data/test_export_csv/csv_test2_result.csv'
+    sep = '\t'
+    converter.convert_to_csv(input_data, output_dir, sep=sep, header=True, is_dir=False)
+    df = read_csv(result_csv, sep=sep)
+    nulls = df.isnull().sum()
+    assert sum(nulls) == 2, "There should be exactly two empty values in result CSV"
+
+    # Ensure fields are valid JSON
+    json.loads(df.iloc[0].writers)
+    json.loads(df.iloc[0].iswcs_1)
+
+    assert open(result_csv).read() == open(assert_csv).read()
+
+
+def test_csv_history():
+    converter = Converter({}, '/tmp')
+    output_dir = '/tmp/lsc-pytest'
+    result_csv = output_dir + '/result.csv'
+    input_data = os.path.abspath(os.path.dirname(__file__)) + '/data/test_export_csv/csv_test_history.json'
+    sep = '\t'
+    converter.convert_to_csv(input_data, output_dir, sep=sep, header=True, is_dir=False)
+    df = read_csv(result_csv, sep=sep)
+    assert 'history' in df.columns, "'history' column is not in the CSV"
