4135af3fa737f19ce72888ca2136229021da5b72
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-06-06 12:49:58 +0000

    SDK regeneration

--- src/label_studio_sdk/core/client_wrapper.py
+++ src/label_studio_sdk/core/client_wrapper.py
@@ -17,7 +17,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "label-studio-sdk",
-            "X-Fern-SDK-Version": "0.0.75",
+            "X-Fern-SDK-Version": "0.0.76",
         }
         headers["Authorization"] = f"Token  {self.api_key}"
         return headers
