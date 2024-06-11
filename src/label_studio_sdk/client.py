864e20e8b2a72e1fa4dfeb488fce1ca913fd1064
nik <nik@heartex.net>
2024-06-11 17:02:07 +0100

    Add empty docstrings

--- src/label_studio_sdk/client.py
+++ src/label_studio_sdk/client.py
@@ -2,8 +2,10 @@ from .base_client import LabelStudioBase, AsyncLabelStudioBase
 
 
 class LabelStudio(LabelStudioBase):
+    """"""
     __doc__ += LabelStudioBase.__doc__
 
 
 class AsyncLabelStudio(AsyncLabelStudioBase):
+    """"""
     __doc__ += AsyncLabelStudioBase.__doc__
