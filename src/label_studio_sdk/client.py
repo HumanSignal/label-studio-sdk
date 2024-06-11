e054a820ad2d6fbb0096bf416f365c876f4be5d0
nik <nik@heartex.net>
2024-06-11 16:57:05 +0100

    Inherit docstrings

--- src/label_studio_sdk/client.py
+++ src/label_studio_sdk/client.py
@@ -2,78 +2,8 @@ from .base_client import LabelStudioBase, AsyncLabelStudioBase
 
 
 class LabelStudio(LabelStudioBase):
-    """
-    Use this class to access the different functions within the SDK. You can instantiate any number of clients with different configuration that will propagate to these functions.
-
-    Parameters
-    ----------
-    base_url : typing.Optional[str]
-        The base url to use for requests from the client.
-
-    environment : LabelStudioEnvironment
-        The environment to use for requests from the client. from .environment import LabelStudioEnvironment
-
-
-
-        Defaults to LabelStudioEnvironment.DEFAULT
-
-
-
-    api_key : typing.Optional[str]
-    timeout : typing.Optional[float]
-        The timeout to be used, in seconds, for requests. By default the timeout is 60 seconds, unless a custom httpx client is used, in which case this default is not enforced.
-
-    follow_redirects : typing.Optional[bool]
-        Whether the default httpx client follows redirects or not, this is irrelevant if a custom httpx client is passed in.
-
-    httpx_client : typing.Optional[httpx.Client]
-        The httpx client to use for making requests, a preconfigured client is used by default, however this is useful should you want to pass in any custom httpx configuration.
-
-    Examples
-    --------
-    from label_studio_sdk.client import LabelStudio
-
-    client = LabelStudio(
-        api_key="YOUR_API_KEY",
-    )
-    """
-    pass
+    __doc__ += LabelStudioBase.__doc__
 
 
 class AsyncLabelStudio(AsyncLabelStudioBase):
-    """
-    Use this class to access the different functions within the SDK. You can instantiate any number of clients with different configuration that will propagate to these functions.
-
-    Parameters
-    ----------
-    base_url : typing.Optional[str]
-        The base url to use for requests from the client.
-
-    environment : LabelStudioEnvironment
-        The environment to use for requests from the client. from .environment import LabelStudioEnvironment
-
-
-
-        Defaults to LabelStudioEnvironment.DEFAULT
-
-
-
-    api_key : typing.Optional[str]
-    timeout : typing.Optional[float]
-        The timeout to be used, in seconds, for requests. By default the timeout is 60 seconds, unless a custom httpx client is used, in which case this default is not enforced.
-
-    follow_redirects : typing.Optional[bool]
-        Whether the default httpx client follows redirects or not, this is irrelevant if a custom httpx client is passed in.
-
-    httpx_client : typing.Optional[httpx.AsyncClient]
-        The httpx client to use for making requests, a preconfigured client is used by default, however this is useful should you want to pass in any custom httpx configuration.
-
-    Examples
-    --------
-    from label_studio_sdk.client import AsyncLabelStudio
-
-    client = AsyncLabelStudio(
-        api_key="YOUR_API_KEY",
-    )
-    """
-    pass
+    __doc__ += AsyncLabelStudioBase.__doc__
