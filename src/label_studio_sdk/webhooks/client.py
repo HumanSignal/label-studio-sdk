81ce1b4ca6ef0dccf756734711e7aee112f25305
makseq-ubnt <makseq@gmail.com>
2024-09-25 02:17:47 +0100

    fix: Webhook tests

--- src/label_studio_sdk/webhooks/client.py
+++ src/label_studio_sdk/webhooks/client.py
@@ -4,6 +4,7 @@ import datetime as dt
 import typing
 from json.decoder import JSONDecodeError
 
+from .types.webhooks_update_request_actions_item import WebhooksUpdateRequestActionsItem
 from ..core.api_error import ApiError
 from ..core.client_wrapper import AsyncClientWrapper, SyncClientWrapper
 from ..core.jsonable_encoder import jsonable_encoder
@@ -12,8 +13,9 @@ from ..core.request_options import RequestOptions
 from ..types.webhook import Webhook
 from ..types.webhook_actions_item import WebhookActionsItem
 from ..types.webhook_serializer_for_update import WebhookSerializerForUpdate
-from ..types.webhook_serializer_for_update_actions_item import WebhookSerializerForUpdateActionsItem
-from .types.webhooks_update_request_actions_item import WebhooksUpdateRequestActionsItem
+from ..types.webhook_serializer_for_update_actions_item import (
+    WebhookSerializerForUpdateActionsItem,
+)
 
 # this is used as the default value for optional parameters
 OMIT = typing.cast(typing.Any, ...)
@@ -24,7 +26,10 @@ class WebhooksClient:
         self._client_wrapper = client_wrapper
 
     def list(
-        self, *, project: typing.Optional[str] = None, request_options: typing.Optional[RequestOptions] = None
+        self,
+        *,
+        project: typing.Optional[str] = None,
+        request_options: typing.Optional[RequestOptions] = None,
     ) -> typing.List[Webhook]:
         """
         List all webhooks set up for your organization.
@@ -56,7 +61,10 @@ class WebhooksClient:
         client.webhooks.list()
         """
         _response = self._client_wrapper.httpx_client.request(
-            "api/webhooks/", method="GET", params={"project": project}, request_options=request_options
+            "api/webhooks/",
+            method="GET",
+            params={"project": project},
+            request_options=request_options,
         )
         try:
             if 200 <= _response.status_code < 300:
@@ -211,7 +219,9 @@ class WebhooksClient:
             raise ApiError(status_code=_response.status_code, body=_response.text)
         raise ApiError(status_code=_response.status_code, body=_response_json)
 
-    def get(self, id: int, *, request_options: typing.Optional[RequestOptions] = None) -> Webhook:
+    def get(
+        self, id: int, *, request_options: typing.Optional[RequestOptions] = None
+    ) -> Webhook:
         """
         Get information about a specific webhook. You will need to provide the webhook ID. You can get this from [List all webhooks](list).
 
@@ -242,7 +252,9 @@ class WebhooksClient:
         )
         """
         _response = self._client_wrapper.httpx_client.request(
-            f"api/webhooks/{jsonable_encoder(id)}/", method="GET", request_options=request_options
+            f"api/webhooks/{jsonable_encoder(id)}/",
+            method="GET",
+            request_options=request_options,
         )
         try:
             if 200 <= _response.status_code < 300:
@@ -252,7 +264,9 @@ class WebhooksClient:
             raise ApiError(status_code=_response.status_code, body=_response.text)
         raise ApiError(status_code=_response.status_code, body=_response_json)
 
-    def delete(self, id: int, *, request_options: typing.Optional[RequestOptions] = None) -> None:
+    def delete(
+        self, id: int, *, request_options: typing.Optional[RequestOptions] = None
+    ) -> None:
         """
         Delete a webhook. You will need to provide the webhook ID. You can get this from [List all webhooks](list).
 
@@ -282,7 +296,9 @@ class WebhooksClient:
         )
         """
         _response = self._client_wrapper.httpx_client.request(
-            f"api/webhooks/{jsonable_encoder(id)}/", method="DELETE", request_options=request_options
+            f"api/webhooks/{jsonable_encoder(id)}/",
+            method="DELETE",
+            request_options=request_options,
         )
         try:
             if 200 <= _response.status_code < 300:
@@ -298,19 +314,26 @@ class WebhooksClient:
         *,
         url: str,
         webhook_serializer_for_update_url: str,
-        send_payload: typing.Optional[bool] = None,
-        send_for_all_actions: typing.Optional[bool] = None,
+        send_payload: bool = True,
+        send_for_all_actions: bool = True,
         headers: typing.Optional[str] = None,
-        is_active: typing.Optional[bool] = None,
+        is_active: bool = True,
         actions: typing.Optional[
-            typing.Union[WebhooksUpdateRequestActionsItem, typing.Sequence[WebhooksUpdateRequestActionsItem]]
+            typing.Union[
+                WebhooksUpdateRequestActionsItem,
+                typing.Sequence[WebhooksUpdateRequestActionsItem],
+            ]
         ] = None,
         id: typing.Optional[int] = OMIT,
         organization: typing.Optional[int] = OMIT,
         project: typing.Optional[int] = OMIT,
         webhook_serializer_for_update_send_payload: typing.Optional[bool] = OMIT,
-        webhook_serializer_for_update_send_for_all_actions: typing.Optional[bool] = OMIT,
-        webhook_serializer_for_update_headers: typing.Optional[typing.Dict[str, typing.Any]] = OMIT,
+        webhook_serializer_for_update_send_for_all_actions: typing.Optional[
+            bool
+        ] = OMIT,
+        webhook_serializer_for_update_headers: typing.Optional[
+            typing.Dict[str, typing.Any]
+        ] = OMIT,
         webhook_serializer_for_update_is_active: typing.Optional[bool] = OMIT,
         webhook_serializer_for_update_actions: typing.Optional[
             typing.Sequence[WebhookSerializerForUpdateActionsItem]
@@ -335,19 +358,19 @@ class WebhooksClient:
         webhook_serializer_for_update_url : str
             URL of webhook
 
-        send_payload : typing.Optional[bool]
+        send_payload : bool
             If value is False send only action
 
-        send_for_all_actions : typing.Optional[bool]
+        send_for_all_actions : bool
             If value is False - used only for actions from WebhookAction
 
-        headers : typing.Optional[str]
+        headers : str
             Key Value Json of headers
 
-        is_active : typing.Optional[bool]
+        is_active : bool
             If value is False the webhook is disabled
 
-        actions : typing.Optional[typing.Union[WebhooksUpdateRequestActionsItem, typing.Sequence[WebhooksUpdateRequestActionsItem]]]
+        actions : typing.Union[WebhooksUpdateRequestActionsItem, typing.Sequence[WebhooksUpdateRequestActionsItem]]
 
         id : typing.Optional[int]
 
@@ -396,6 +419,8 @@ class WebhooksClient:
             webhook_serializer_for_update_url="url",
         )
         """
+        actions = [] if actions is None else actions
+        headers = {} if headers is None else headers
         _response = self._client_wrapper.httpx_client.request(
             f"api/webhooks/{jsonable_encoder(id_)}/",
             method="PATCH",
@@ -437,7 +462,10 @@ class AsyncWebhooksClient:
         self._client_wrapper = client_wrapper
 
     async def list(
-        self, *, project: typing.Optional[str] = None, request_options: typing.Optional[RequestOptions] = None
+        self,
+        *,
+        project: typing.Optional[str] = None,
+        request_options: typing.Optional[RequestOptions] = None,
     ) -> typing.List[Webhook]:
         """
         List all webhooks set up for your organization.
@@ -469,7 +497,10 @@ class AsyncWebhooksClient:
         await client.webhooks.list()
         """
         _response = await self._client_wrapper.httpx_client.request(
-            "api/webhooks/", method="GET", params={"project": project}, request_options=request_options
+            "api/webhooks/",
+            method="GET",
+            params={"project": project},
+            request_options=request_options,
         )
         try:
             if 200 <= _response.status_code < 300:
@@ -624,7 +655,9 @@ class AsyncWebhooksClient:
             raise ApiError(status_code=_response.status_code, body=_response.text)
         raise ApiError(status_code=_response.status_code, body=_response_json)
 
-    async def get(self, id: int, *, request_options: typing.Optional[RequestOptions] = None) -> Webhook:
+    async def get(
+        self, id: int, *, request_options: typing.Optional[RequestOptions] = None
+    ) -> Webhook:
         """
         Get information about a specific webhook. You will need to provide the webhook ID. You can get this from [List all webhooks](list).
 
@@ -655,7 +688,9 @@ class AsyncWebhooksClient:
         )
         """
         _response = await self._client_wrapper.httpx_client.request(
-            f"api/webhooks/{jsonable_encoder(id)}/", method="GET", request_options=request_options
+            f"api/webhooks/{jsonable_encoder(id)}/",
+            method="GET",
+            request_options=request_options,
         )
         try:
             if 200 <= _response.status_code < 300:
@@ -665,7 +700,9 @@ class AsyncWebhooksClient:
             raise ApiError(status_code=_response.status_code, body=_response.text)
         raise ApiError(status_code=_response.status_code, body=_response_json)
 
-    async def delete(self, id: int, *, request_options: typing.Optional[RequestOptions] = None) -> None:
+    async def delete(
+        self, id: int, *, request_options: typing.Optional[RequestOptions] = None
+    ) -> None:
         """
         Delete a webhook. You will need to provide the webhook ID. You can get this from [List all webhooks](list).
 
@@ -695,7 +732,9 @@ class AsyncWebhooksClient:
         )
         """
         _response = await self._client_wrapper.httpx_client.request(
-            f"api/webhooks/{jsonable_encoder(id)}/", method="DELETE", request_options=request_options
+            f"api/webhooks/{jsonable_encoder(id)}/",
+            method="DELETE",
+            request_options=request_options,
         )
         try:
             if 200 <= _response.status_code < 300:
@@ -716,14 +755,21 @@ class AsyncWebhooksClient:
         headers: typing.Optional[str] = None,
         is_active: typing.Optional[bool] = None,
         actions: typing.Optional[
-            typing.Union[WebhooksUpdateRequestActionsItem, typing.Sequence[WebhooksUpdateRequestActionsItem]]
+            typing.Union[
+                WebhooksUpdateRequestActionsItem,
+                typing.Sequence[WebhooksUpdateRequestActionsItem],
+            ]
         ] = None,
         id: typing.Optional[int] = OMIT,
         organization: typing.Optional[int] = OMIT,
         project: typing.Optional[int] = OMIT,
         webhook_serializer_for_update_send_payload: typing.Optional[bool] = OMIT,
-        webhook_serializer_for_update_send_for_all_actions: typing.Optional[bool] = OMIT,
-        webhook_serializer_for_update_headers: typing.Optional[typing.Dict[str, typing.Any]] = OMIT,
+        webhook_serializer_for_update_send_for_all_actions: typing.Optional[
+            bool
+        ] = OMIT,
+        webhook_serializer_for_update_headers: typing.Optional[
+            typing.Dict[str, typing.Any]
+        ] = OMIT,
         webhook_serializer_for_update_is_active: typing.Optional[bool] = OMIT,
         webhook_serializer_for_update_actions: typing.Optional[
             typing.Sequence[WebhookSerializerForUpdateActionsItem]
