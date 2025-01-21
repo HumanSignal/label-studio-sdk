bbc2aef107abc5dba8d5fef3a8f600b945e0ffb8
Sergei Ivashchenko <triklozoid@gmail.com>
2025-01-21 11:32:26 +0000

    fix: LEAP-1692: Image export for COCO and YOLO (#383)
    Co-authored-by: Max Tkachenko <makseq@gmail.com>

--- src/label_studio_sdk/_extensions/label_studio_tools/core/utils/io.py
+++ src/label_studio_sdk/_extensions/label_studio_tools/core/utils/io.py
@@ -124,9 +124,9 @@ def get_local_path(
     if is_uploaded_file and os.path.exists(image_dir):
         project_id = url.split("/")[-2]  # To retrieve project_id
         filepath = os.path.join(image_dir, project_id, os.path.basename(url))
-        if cache_dir and download_resources:
-            shutil.copy(filepath, cache_dir)
         if os.path.exists(filepath):
+            if cache_dir and download_resources:
+                shutil.copy(filepath, cache_dir)
             logger.debug(f"Uploaded file: Path exists in image_dir: {filepath}")
             return filepath
 
@@ -202,7 +202,7 @@ def download_and_cache(
     filepath = os.path.join(cache_dir, url_hash + "__" + url_filename)
 
     if not os.path.exists(filepath):
-        logger.info("Download {url} to {filepath}".format(url=url, filepath=filepath))
+        logger.info("Download {url} to {filepath}. download_resources: {download_resources}".format(url=url, filepath=filepath, download_resources=download_resources))
         if download_resources:
             headers = {
                 # avoid requests.exceptions.HTTPError: 403 Client Error: Forbidden. Please comply with the User-Agent policy:
@@ -227,6 +227,7 @@ def download_and_cache(
                 raise e
             with io.open(filepath, mode="wb") as fout:
                 fout.write(r.content)
+                logger.info(f"File downloaded to {filepath}")
     return filepath
 
 
