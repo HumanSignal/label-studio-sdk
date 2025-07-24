4645c8db98146d4f6a6efb263b0bf7637ec67475
Jo Booth <jo.m.booth@gmail.com>
2025-07-24 01:40:11 -0400

    fix arguments issue in exports client

--- src/label_studio_sdk/projects/exports/client_ext.py
+++ src/label_studio_sdk/projects/exports/client_ext.py
@@ -10,7 +10,7 @@ from label_studio_sdk.core.api_error import ApiError
 
 
 class ExportTimeoutError(ApiError):
-    
+
     def __init__(self, export_snapshot):
         super().__init__(
             status_code=500,
@@ -22,9 +22,9 @@ class ExportTimeoutError(ApiError):
                 f"`ls.projects.exports.get(project_id={project_id}, export_pk={export_snapshot.id})`."
             )
         )
-        
+
 class ExportFailedError(ApiError):
-    
+
     def __init__(self, export_snapshot):
         super().__init__(
             status_code=500,
@@ -44,39 +44,39 @@ def _check_status(export_snapshot, converted_format_id, status):
 
 
 class ExportsClientExt(ExportsClient):
-    
+
     def _bytestream_to_fileobj(self, bytestream: typing.Iterable[bytes] | bytes) -> typing.BinaryIO:
         buffer = BytesIO()
-        if isinstance(bytestream, typing.Iterable): 
+        if isinstance(bytestream, typing.Iterable):
             for chunk in bytestream:
                 buffer.write(chunk)
         else:
             buffer.write(bytestream)
         buffer.seek(0)
         return buffer
-    
+
     def _bytestream_to_binary(self, bytestream: typing.Iterable[bytes]) -> bytes:
         fileobj = self._bytestream_to_fileobj(bytestream)
         return fileobj.getvalue()
-    
+
     def _bytestream_to_json(self, bytestream: typing.Iterable[bytes]) -> dict:
         fileobj = self._bytestream_to_fileobj(bytestream)
         return json.load(fileobj)
-    
+
     def _bytestream_to_pandas(self, bytestream: typing.Iterable[bytes]) -> pd.DataFrame:
         fileobj = self._bytestream_to_fileobj(bytestream)
         return pd.read_csv(fileobj)
-    
+
     def _poll_export(self, project_id, export_snapshot, converted_format_id, timeout):
         start_time = time.time()
         while not _check_status(export_snapshot, None, 'completed'):
-            export_snapshot = self.get(project_id, export_pk=export_snapshot.id)
+            export_snapshot = self.get(id=project_id, export_pk=export_snapshot.id)
             if _check_status(export_snapshot, None, 'failed'):
                 raise ExportFailedError(export_snapshot)
             if time.time() - start_time > timeout:
                 raise ExportTimeoutError(export_snapshot)
             time.sleep(1)
-    
+
     def _get_bytestream(
         self,
         project_id: int,
@@ -87,7 +87,7 @@ class ExportsClientExt(ExportsClient):
         download_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None,
     ):
         version = VersionsClient(client_wrapper=self._client_wrapper).get()
-        
+
         if version.edition == "Enterprise":
             # Enterprise edition exports are async, so we need to wait for the export job to complete
             export_snapshot = self.create(project_id, **(create_kwargs or {}))
@@ -97,31 +97,31 @@ class ExportsClientExt(ExportsClient):
             if export_type != "JSON":
                 converted_proc = self.convert(project_id, export_pk=export_snapshot.id, export_type=export_type, **(convert_kwargs or {}))
                 self._poll_export(project_id, export_snapshot, converted_proc.converted_format, timeout)
-                
+
             bytestream = self.download(project_id, export_pk=export_snapshot.id, export_type=export_type, request_options={'chunk_size': 1024}, **(download_kwargs or {}))
         else:
             # Community edition exports are sync, so we can download the file immediately
             bytestream = self.download_sync(project_id, export_type=export_type, download_all_tasks=True, download_resources=True)
         return bytestream
-    
+
     def as_file(self, project_id: int, export_type: str = "JSON", timeout: int = 60, create_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, convert_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, download_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None):
         bytestream = self._get_bytestream(project_id, export_type, timeout, create_kwargs, convert_kwargs, download_kwargs)
         return self._bytestream_to_fileobj(bytestream)
-    
+
     def as_binary(self, project_id: int, export_type: str = "JSON", timeout: int = 60, create_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, convert_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, download_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None):
         bytestream = self._get_bytestream(project_id, export_type, timeout, create_kwargs, convert_kwargs, download_kwargs)
         return self._bytestream_to_binary(bytestream)
-    
+
     def as_json(self, project_id: int, timeout: int = 60, create_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, convert_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, download_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None):
         bytestream = self._get_bytestream(project_id, "JSON", timeout, create_kwargs, convert_kwargs, download_kwargs)
         return self._bytestream_to_json(bytestream)
-    
+
     def as_pandas(self, project_id: int, timeout: int = 60, create_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, convert_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, download_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None):
         bytestream = self._get_bytestream(project_id, "CSV", timeout, create_kwargs, convert_kwargs, download_kwargs)
         return self._bytestream_to_pandas(bytestream)
-    
+
 class AsyncExportsClientExt(AsyncExportsClient):
-    
+
     async def _bytestream_to_fileobj(self, bytestream: typing.AsyncGenerator[bytes, None] | bytes):
         """Convert bytestream to file-like object"""
         fileobj = BytesIO()
@@ -147,11 +147,11 @@ class AsyncExportsClientExt(AsyncExportsClient):
         """Convert bytestream to pandas DataFrame"""
         fileobj = await self._bytestream_to_fileobj(bytestream)
         return pd.read_csv(fileobj)
-    
+
     async def _poll_export(self, project_id, export_snapshot, converted_format_id, timeout):
         start_time = time.time()
         while not _check_status(export_snapshot, None, 'completed'):
-            export_snapshot = await self.get(project_id, export_pk=export_snapshot.id)
+            export_snapshot = await self.get(id=project_id, export_pk=export_snapshot.id)
             if _check_status(export_snapshot, None, 'failed'):
                 raise ExportFailedError(export_snapshot)
             if time.time() - start_time > timeout:
@@ -177,24 +177,24 @@ class AsyncExportsClientExt(AsyncExportsClient):
             if export_type != "JSON":
                 converted_proc = await self.convert(project_id, export_pk=export_snapshot.id, export_type=export_type, **(convert_kwargs or {}))
                 await self._poll_export(project_id, export_snapshot, converted_proc.converted_format, timeout)
-            
+
             bytestream = self.download(project_id, export_pk=export_snapshot.id, export_type=export_type, request_options={'chunk_size': 1024}, **(download_kwargs or {}))
         else:
             bytestream = self.download_sync(project_id, export_type=export_type, download_all_tasks=True, download_resources=True)
         return bytestream
-    
+
     async def as_file(self, project_id: int, export_type: str = "JSON", timeout: int = 60, create_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, convert_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, download_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None):
         bytestream = await self._get_bytestream(project_id, export_type, timeout, create_kwargs, convert_kwargs, download_kwargs)
         return await self._bytestream_to_fileobj(bytestream)
-    
+
     async def as_binary(self, project_id: int, export_type: str = "JSON", timeout: int = 60, create_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, convert_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, download_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None):
         bytestream = await self._get_bytestream(project_id, export_type, timeout, create_kwargs, convert_kwargs, download_kwargs)
         return await self._bytestream_to_binary(bytestream)
-    
+
     async def as_json(self, project_id: int, timeout: int = 60, create_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, convert_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, download_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None):
         bytestream = await self._get_bytestream(project_id, "JSON", timeout, create_kwargs, convert_kwargs, download_kwargs)
         return await self._bytestream_to_json(bytestream)
-    
+
     async def as_pandas(self, project_id: int, timeout: int = 60, create_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, convert_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None, download_kwargs: typing.Optional[typing.Dict[str, typing.Any]] = None):
         bytestream = await self._get_bytestream(project_id, "CSV", timeout, create_kwargs, convert_kwargs, download_kwargs)
         return await self._bytestream_to_pandas(bytestream)
