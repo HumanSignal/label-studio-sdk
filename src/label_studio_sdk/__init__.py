a9cd3513e7093cfb829e9968283d148c0ce955ae
Jo Booth <jo.m.booth@gmail.com>
2025-07-29 15:50:30 -0400

    merge cleanup

--- src/label_studio_sdk/__init__.py
+++ src/label_studio_sdk/__init__.py
@@ -225,25 +225,8 @@ from .projects import (
     ProjectsImportTasksResponse,
     ProjectsListRequestFilter,
 )
-<<<<<<< HEAD
 from .prompts import PromptsCompatibleProjectsRequestProjectType
 from .tasks import TasksListRequestFields
-=======
-from .projects import (
-    ProjectsCreateResponse,
-    ProjectsImportTasksResponse,
-    ProjectsListRequestFilter,
-    ProjectsListResponse,
-    ProjectsUpdateResponse,
-)
-from .prompts import (
-    PromptsBatchFailedPredictionsRequestFailedPredictionsItem,
-    PromptsBatchFailedPredictionsResponse,
-    PromptsBatchPredictionsRequestResultsItem,
-    PromptsBatchPredictionsResponse,
-)
-from .tasks import TasksListRequestFields, TasksListResponse
->>>>>>> master
 from .users import UsersGetTokenResponse, UsersResetTokenResponse
 from .version import __version__
 from .views import (
@@ -438,27 +421,9 @@ __all__ = [
     "ProjectsDuplicateResponse",
     "ProjectsImportTasksResponse",
     "ProjectsListRequestFilter",
-<<<<<<< HEAD
     "PromptsCompatibleProjectsRequestProjectType",
     "ProviderEnum",
     "ReasonEnum",
-=======
-    "ProjectsListResponse",
-    "ProjectsUpdateResponse",
-    "Prompt",
-    "PromptAssociatedProjectsItem",
-    "PromptAssociatedProjectsItemId",
-    "PromptCreatedBy",
-    "PromptOrganization",
-    "PromptVersion",
-    "PromptVersionCreatedBy",
-    "PromptVersionOrganization",
-    "PromptVersionProvider",
-    "PromptsBatchFailedPredictionsRequestFailedPredictionsItem",
-    "PromptsBatchFailedPredictionsResponse",
-    "PromptsBatchPredictionsRequestResultsItem",
-    "PromptsBatchPredictionsResponse",
->>>>>>> master
     "RedisExportStorage",
     "RedisImportStorage",
     "RefinedPromptResponse",
