8117c8e09b48db2456fdb6dc65a34c73bbf939d9
Jo Booth <jo@humansignal.com>
2025-09-25 18:47:15 -0400

    fix import in client_ext

--- src/label_studio_sdk/projects/client_ext.py
+++ src/label_studio_sdk/projects/client_ext.py
@@ -3,21 +3,21 @@ from typing_extensions import Annotated
 from .client import ProjectsClient, AsyncProjectsClient
 from pydantic import model_validator, validator, Field, ConfigDict
 from label_studio_sdk._extensions.pager_ext import SyncPagerExt, AsyncPagerExt, T
-from label_studio_sdk.types.project import Project
+from label_studio_sdk.types.lse_project_response import LseProjectResponse
 from label_studio_sdk.label_interface import LabelInterface
 from .exports.client_ext import ExportsClientExt, AsyncExportsClientExt
 
 from ..core import RequestOptions
 
 
-class ProjectExt(Project):
+class ProjectExt(LseProjectResponse):
 
     def get_label_interface(self):
         return LabelInterface(self.label_config)
 
 
 class ProjectsClientExt(ProjectsClient):
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.exports = ExportsClientExt(client_wrapper=self._client_wrapper)
@@ -29,7 +29,7 @@ class ProjectsClientExt(ProjectsClient):
 
     def get(self, id: int, *, request_options: typing.Optional[RequestOptions] = None) -> ProjectExt:
         return ProjectExt(**dict(super().get(id, request_options=request_options)))
-    
+
     get.__doc__ = ProjectsClient.get.__doc__
 
 
@@ -41,7 +41,7 @@ class AsyncProjectsClientExt(AsyncProjectsClient):
 
     async def get(self, id: int, *, request_options: typing.Optional[RequestOptions] = None) -> ProjectExt:
         return ProjectExt(**dict(await super().get(id, request_options=request_options)))
-    
+
     get.__doc__ = AsyncProjectsClient.get.__doc__
 
     async def list(self, **kwargs):
